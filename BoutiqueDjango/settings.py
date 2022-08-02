18e6b5d6c8a3e46fef352634f7da9a0505d65279
Ben Smith <benjaminsmith1981@googlemail.com>
2022-08-02 16:02:33 +0000

    Added login bits

--- BoutiqueDjango/settings.py
+++ BoutiqueDjango/settings.py
@@ -80,10 +80,20 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
     
 ]
+
 SITE_ID = 1
 
-WSGI_APPLICATION = 'BoutiqueDjango.wsgi.application'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/success'
+
+WSGI_APPLICATION = 'BoutiqueDjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
