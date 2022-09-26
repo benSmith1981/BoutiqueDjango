300494d46baf711469aaf0be400bde850522b137
Ben Smith <benjaminsmith1981@googlemail.com>
2022-09-26 15:55:46 +0100

    Updated showing bag contents

--- bag/views.py
+++ bag/views.py
@@ -10,14 +10,12 @@ def add_to_bag(request, item_id):
     #add quantiy of specified product
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-
     bag = request.session.get('bag',{})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
-        bag[item_id] = quantity 
+        bag[item_id] = quantity
     
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
